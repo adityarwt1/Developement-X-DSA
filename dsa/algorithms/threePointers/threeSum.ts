0302510c5c0b0fb866192be4b1fcf382ca195b85
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-03-20 22:58:58 +0530

    threesum Problem resolve by makin the least pointer in life

--- dsa/algorithms/threePointers/threeSum.ts
+++ dsa/algorithms/threePointers/threeSum.ts
@@ -15,11 +15,35 @@ const threeSumBruteForce = (nums:number[], target:number):number[]=>{
 console.log(threeSumBruteForce([-1,0,1,2,-1,-4], 5))
 
 /// making thhe triplet whose sum is equal to the 0
-function threeSum(nums:number[]):number[]{
-    const set = new Set()
+const threeSum = (nums:number[]):number[][]=>{
+    // sorting the array 
+    nums.sort((a,b)=> a-b)
+    const result :number[][] = []
 
-    let left = 0
-    for(let right = left +1 ; right < nums.length ; right++){
-        
+    const n : number = nums.length
+
+    for(let i : number = 0 ; i < n-2; i++){
+        // skipping the duplicate 
+        if(i > 0 && nums[i]=== nums[i-1]) continue; 
+        if(nums[i]> 0) break;
+
+        let L:number = i + 1
+        let R:number = n -1
+
+        while(L < R){
+            const sum = nums[i] + nums[L]+ nums[R]
+            if(sum === 0){
+                result.push([nums[i], nums[L], nums[R]])
+            while(L<R && nums[L]=== nums[L+1]) L++
+            while(L<R && nums[R] === nums[R-1])R++;
+                L++
+                R--
+            } else if(sum< 0) {
+                L++ // for increase the number
+            } else {
+                R--
+            }
+        }
     }
+    return result
 }
